0f6b5765c6f2fcec169ea385bfa7787c4ccc5361
unknown <wenjin001@berryoncology.com>
2020-05-14 17:34:04 +0800

    modified:   my-app/src/components/NavLeft/index.css 	modified:   my-app/src/components/NavLeft/index.js 	modified:   my-app/src/style/common.less 	my-app/src/config/

--- my-app/src/components/NavLeft/index.js
+++ my-app/src/components/NavLeft/index.js
@@ -1,6 +1,32 @@
 import React from 'react' 
+import {Menu, Icon} from 'antd'
+import MenuConfig from './../../config/menuConfig'
+
+import './index.css'
+const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component{
+
+    componentWillMount(){
+       const menuTreeNode = this.renderMenu(MenuConfig);
+       this.setState={
+           menuTreeNode
+       }
+    }
+
+    //菜单渲染
+    renderMenu=(data)=>{
+        return data.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+        return <Menu.item title={item.title} key={item.key}>{item.title}</Menu.item>
+        })
+    }
 
     render(){
  
@@ -8,7 +34,13 @@ export default class NavLeft extends React.Component{
 
 
             <div >
-                This is NavLeft
+                <div className="logo">
+                    <img src="logo192.png" alt=""></img>
+                    <h1>学习React</h1>
+                </div>
+                <Menu theme="dark">
+                    {this.state.menuTreeNode}
+                </Menu>
             </div>
         );
     }
