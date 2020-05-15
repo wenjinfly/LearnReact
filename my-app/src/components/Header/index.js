bd68888b4d72ed8c2d4cf6d846ce3df906724f11
unknown <wenjin001@berryoncology.com>
2020-05-15 17:31:33 +0800

    modified:   my-app/src/components/Header/index.css 	modified:   my-app/src/components/Header/index.js 	modified:   my-app/src/components/NavLeft/index.css 	modified:   my-app/src/components/NavLeft/index.js 	modified:   my-app/src/style/common.less 	my-app/src/config/ 	my-app/src/utils/

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,12 +1,33 @@
 import React from 'react' 
+import { Row, Col } from 'antd'
 
+import './index.css'
 export default class Header extends React.Component{
 
+    componentWillMount(){
+        this.setState({
+            userName:"麦子"
+        })
+    };
     render(){
         return (
 
-            <div>
-                This is Herder
+            <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span="20" className="weather">
+                        <span>2020-05-1</span>
+                        <span>hage</span>
+                    </Col>
+                </Row>
             </div>
         );
     }
--- my-app/src/components/NavLeft/index.js
+++ my-app/src/components/NavLeft/index.js
@@ -9,39 +9,40 @@ export default class NavLeft extends React.Component{
 
     componentWillMount(){
        const menuTreeNode = this.renderMenu(MenuConfig);
-       this.setState={
-           menuTreeNode
-       }
-    }
+       this.setState({
+            menuTreeNode
+        })
+    };
 
-    //菜单渲染
-    renderMenu=(data)=>{
+ 
+    // 菜单渲染
+    renderMenu =(data)=>{
         return data.map((item)=>{
             if(item.children){
                 return (
                     <SubMenu title={item.title} key={item.key}>
-                        {this.renderMenu(item.children)}
+                        { this.renderMenu(item.children)}
                     </SubMenu>
                 )
             }
-        return <Menu.item title={item.title} key={item.key}>{item.title}</Menu.item>
+            return <Menu.Item title={item.title} key={item.key} >
+                {item.title}
+            </Menu.Item>
         })
     }
 
     render(){
  
         return (
-
-
             <div >
                 <div className="logo">
                     <img src="logo192.png" alt=""></img>
-                    <h1>学习React</h1>
+                    <h1 className="h1St">学习React</h1>
                 </div>
-                <Menu theme="dark">
+                <Menu theme="dark" >
                     {this.state.menuTreeNode}
                 </Menu>
             </div>
         );
     }
-}
+} //
